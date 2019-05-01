daa6e8fd6e26d4572c761a21a56d81d1968c3ffb
juztinfied <z_d_143@hotmail.com>
2019-05-01 17:21:28 -0500

    minor changes

---
+++ src/Config/config.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBEWLtwoWqSSFZvp7NoIyGUsPKpMa9hOdQ",
+    authDomain: "new-shopping-cart-1db9a.firebaseapp.com",
+    databaseURL: "https://new-shopping-cart-1db9a.firebaseio.com",
+    projectId: "new-shopping-cart-1db9a",
+    storageBucket: "new-shopping-cart-1db9a.appspot.com",
+    messagingSenderId: "978677052518"
+  };
+  firebase.initializeApp(config);
--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,12 @@ const Card = ({title, pathName, price, handleClickProduct}) => {
             <img src={process.env.PUBLIC_URL + pathName} title={[title,price]} onClick={handleClickProduct} />
             <div>{title}</div>
             <div>{price}</div>
-            <div className="AddToCart">Add To Cart</div>
+            <div className="AddToCart">Add To Cart
+                <button className="s-button">S</button>
+                <button className="m-button">M</button>
+                <button className="l-button">L</button>
+                <button className="xl-button">XL</button>
+            </div>
         </div>
     )
 
